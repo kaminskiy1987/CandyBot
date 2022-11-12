0f85141c6c09e16c98c63a83edb88c0c4078c811
Mikhail <m.kaminskiy@list.ru>
2022-11-12 19:55:30 +0700

    bot.py

---
+++ bot.py
@@ -0,0 +1,8 @@
+#Здесь создается бот и хранится его токен
+
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+
+bot = Bot(token='5478500058:AAHkVgklz9m-8m1B5yoqv6T_ByRf6UFh_30')
+dp = Dispatcher(bot)
